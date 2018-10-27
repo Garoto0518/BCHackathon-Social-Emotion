60b6b20525886418bc9e4f493cc66993cef6ad5e
Gabriel Rodriguez - Torres <gabrielrguez0518@gmail.com>
2018-10-27 08:21:47 -0400

    Finish Product For BCHackathon, Social Emotion.

---
+++ app/src/main/java/com/garoto/bchackathon/ComplaintsActivity.java
@@ -0,0 +1,13 @@
+package com.garoto.bchackathon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ComplaintsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.complaints_activity);
+    }
+}
